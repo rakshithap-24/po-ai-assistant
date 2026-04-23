5eaa00e8869e7f7da77be46a4e64a998aed7ad4c
Rakshitha Prakash <rprakash@ttu.edu>
2026-04-23 05:44:06 +0000

    Fix CAP action handler mapping and validate approvePO flow

--- srv/service.js
+++ srv/service.js
@@ -1,9 +1,9 @@
 const cds = require('@sap/cds')
 
 module.exports = cds.service.impl(async function () {
-  const { PurchaseOrders } = this.entities
+  const { PurchaseOrder } = this.entities
 
-  this.before('CREATE', 'PurchaseOrders', async (req) => {
+  this.before('CREATE', 'PurchaseOrder', async (req) => {
     if (!req.data.status) {
       req.data.status = 'Draft'
     }
@@ -13,24 +13,24 @@ module.exports = cds.service.impl(async function () {
     }
   })
 
-  this.on('ApprovePO', async (req) => {
+  this.on('approvePO', async (req) => {
     const { ID } = req.data
     const tx = cds.tx(req)
 
-    const po = await tx.read(PurchaseOrders).where({ ID })
+    const po = await tx.read(PurchaseOrder).where({ ID })
 
     if (!po.length) {
       req.error(404, 'Purchase Order not found')
     }
 
-    await tx.update(PurchaseOrders)
+    await tx.update(PurchaseOrder)
       .set({ status: 'Approved' })
       .where({ ID })
 
     return `PO ${ID} approved successfully`
   })
 
-  this.on('GeneratePOInsight', async (req) => {
+  this.on('generatePOInsight', async (req) => {
     const { ID } = req.data
     return `AI insight placeholder for PO ${ID}`
   })
